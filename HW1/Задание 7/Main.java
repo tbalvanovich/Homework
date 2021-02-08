366417123509b0114e79139c29821ad79c3f2e98
tbalvanovich <78137299+tbalvanovich@users.noreply.github.com>
2021-02-08 18:16:47 +0300

    Update Main.java
    Add correct code

--- HW1/Задание 7/Main.java
+++ HW1/Задание 7/Main.java
@@ -1,11 +1,11 @@
 public class Main{
     public static void main(String[] args) {
-	int a=4;
-	int b=6;
-	int r=5;
+	double a = 4;
+	double b = 6;
+	double r = 5;
 
  	
-	if ( a*a + b*b > r*r )
+	if ( a * a + b * b > r * r )
 		System.out.println ("possible");
 	else
 	System.out.println ("unpossible");
